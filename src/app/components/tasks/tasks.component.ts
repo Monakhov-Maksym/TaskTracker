09216ddcd65d68173b14d3d1d113b3a70d478b09
Monakhov-Maksym <monakhov.maksym@gmail.com>
2023-07-21 17:55:07 +0300

    some new code

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { TASKS } from '../../mock-tests';
+import { Component, OnInit } from '@angular/core';
+
+import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
 
 @Component({
@@ -7,6 +8,18 @@ import { Task } from '../../Task';
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
 }
