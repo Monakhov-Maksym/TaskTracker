a300f77e8b02276a9f7e8bf631fef9a2c15f20fc
Monakhov-Maksym <monakhov.maksym@gmail.com>
2023-07-21 14:52:01 +0300

    some new code

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { TASKS } from '../../mock-tests';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent {
-
+  tasks: Task[] = TASKS;
 }
